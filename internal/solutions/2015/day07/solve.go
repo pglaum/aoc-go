b02849626e90fe9bbf631ebf7d1b25f9de5b3106
Philipp Glaum <p@pglaum.de>
2025-12-12 08:44:46 +0100

    2015/7

--- internal/solutions/2015/day07/solve.go
+++ internal/solutions/2015/day07/solve.go
@@ -4,7 +4,12 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"slices"
+	"strconv"
+	"strings"
 	"time"
+
+	"github.com/pglaum/aoc-go/internal/stringutils"
 )
 
 var (
@@ -34,12 +39,113 @@ func main() {
 	}
 }
 
-func part1(puzzle string) any {
-	_ = puzzle
-	return 0
+var circuit map[string]uint16
+
+func getValue(s string) uint16 {
+	num, err := strconv.ParseUint(s, 10, 16)
+	if err == nil {
+		return uint16(num)
+	}
+	val, ok := circuit[s]
+	if !ok {
+		fmt.Printf("stack value not found: %q\n", s)
+	}
+	return val
+}
+func canGetValue(s string) bool {
+	_, err := strconv.ParseUint(s, 10, 16)
+	if err == nil {
+		return true
+	}
+	_, ok := circuit[s]
+	return ok
+}
+
+func runWire(wiring []string, stack string) {
+	switch len(wiring) {
+	case 1:
+		circuit[stack] = getValue(wiring[0])
+	case 2:
+		if wiring[0] == "NOT" {
+			circuit[stack] = ^getValue(wiring[1])
+		} else {
+			fmt.Printf("unexpected wiring(2): %v -> %s\n", wiring, stack)
+		}
+	case 3:
+		value1 := getValue(wiring[0])
+		value2 := getValue(wiring[2])
+		switch wiring[1] {
+		case "AND":
+			circuit[stack] = value1 & value2
+		case "OR":
+			circuit[stack] = value1 | value2
+		case "LSHIFT":
+			circuit[stack] = value1 << value2
+		case "RSHIFT":
+			circuit[stack] = value1 >> value2
+		}
+	}
+}
+
+func canRunWire(wiring []string) bool {
+	switch len(wiring) {
+	case 1:
+		return canGetValue(wiring[0])
+	case 2:
+		return canGetValue(wiring[1])
+	case 3:
+		return canGetValue(wiring[0]) && canGetValue(wiring[2])
+	}
+	return false
+}
+
+func runSimulation(lines []string, skipBAssignment bool) {
+	doneWires := []int{}
+	for {
+		for i := range lines {
+			if slices.Contains(doneWires, i) {
+				continue
+			}
+			line := lines[i]
+			splitWiring := strings.Split(line, " -> ")
+			wiring := strings.Fields(splitWiring[0])
+			stack := splitWiring[1]
+
+			if !canRunWire(wiring) {
+				continue
+			}
+
+			if stack == "b" && len(wiring) == 1 && skipBAssignment {
+				doneWires = append(doneWires, i)
+				continue
+			}
+
+			runWire(wiring, stack)
+			doneWires = append(doneWires, i)
+		}
+		if len(doneWires) >= len(lines) {
+			break
+		}
+	}
+}
+
+func part1(puzzle string) int {
+	lines := stringutils.SplitLines(puzzle, true)
+	circuit = make(map[string]uint16)
+	runSimulation(lines, false)
+	return int(circuit["a"])
 }
 
 func part2(puzzle string) any {
-	_ = puzzle
-	return 0
+	lines := stringutils.SplitLines(puzzle, true)
+	circuit = make(map[string]uint16)
+
+	runSimulation(lines, false)
+
+	b := circuit["a"]
+	circuit = make(map[string]uint16)
+	circuit["b"] = b
+	runSimulation(lines, true)
+
+	return int(circuit["a"])
 }
--- internal/solutions/2015/day07/solve_test.go
+++ internal/solutions/2015/day07/solve_test.go
@@ -6,21 +6,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var circuit = `123 -> x
-456 -> y
-x AND y -> d
-x OR y -> e
+var circuitSpec = `123 -> x
+456 -> b
+x AND b -> d
+x OR b -> e
 x LSHIFT 2 -> f
-y RSHIFT 2 -> g
+b RSHIFT 2 -> g
 NOT x -> h
-NOT y -> i
+NOT b -> a
 `
 
 func TestSolve_2015_Day01(t *testing.T) {
 	t.Run("part 1", func(t *testing.T) {
-		assert.Equal(t, 2, part1(circuit))
+		assert.Equal(t, 65079, part1(circuitSpec))
 	})
 	t.Run("part 2", func(t *testing.T) {
-		assert.Equal(t, 2, part2(circuit))
+		assert.Equal(t, 456, part2(circuitSpec))
 	})
 }
